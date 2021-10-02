5294f4bcec6101fceed84094a0bf923524e89416
Shubhankar <shubhankardassan@gmail.com>
2021-10-02 16:47:37 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        int a = 5;
+        int b = 11;
+        // System.out.println("The Sum is "+(a+b));
+        System.out.println("The Multiplication is " (a*b));
+    }
+}
