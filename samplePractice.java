f527c5fd9aca5a5c52ae9ea76f4ae7698f278c10
Shubhankar <shubhankardassan@gmail.com>
2021-10-02 07:45:41 +0530

    added all practice files

--- samplePractice.java
+++ samplePractice.java
@@ -1,5 +1,8 @@
 public class samplePractice{
     public static void main(String args[]){
         System.out.println("Hello World");
+        int a = 5;
+        int b = 11;
+        System.out.println("The Sum is "+(a+b));
     }
 }
